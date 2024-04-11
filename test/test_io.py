c61453cc4c3dfa3eac8132e072a7c07b7b847f1b
Neville Yee <neville.yee@alumni.york.ac.uk>
2024-04-11 13:36:39 +0000

    Moved preparatory operations to setup subroutine in test_io.

--- test/test_io.py
+++ test/test_io.py
@@ -32,42 +32,52 @@ from GeoLlama import io
 
 
 class IOSmokeTest(unittest.TestCase):
-    def _create_dummy_image(self):
-        tmpdir = tempfile.TemporaryDirectory()
 
-        test_data = np.full(
+    @classmethod
+    def setUpClass(self):
+        pass
+
+
+    def setUp(self):
+        self.tmpdir = tempfile.TemporaryDirectory()
+
+        self.test_data = np.full(
             shape = (100, 100, 100),
             fill_value = 1.0,
             dtype = np.float32
         )
 
-        tmp_address = f"{tmpdir.name}/test.mrc"
+        tmp_address = f"{self.tmpdir.name}/test.mrc"
         with mrcfile.new(tmp_address) as f:
-            f.set_data(test_data)
-
-        return tmpdir, test_data
+            f.set_data(self.test_data)
 
 
     def test_read_mrc(self):
-        tmpdir, raw_img = self._create_dummy_image()
         test_px_size = 1.0
 
-        os.chdir(tmpdir.name)
+        os.chdir(self.tmpdir.name)
         # Test whether image remains unchanged if no binning
         data, px_size = io.read_mrc(fname="./test.mrc",
                                     px_size_nm=test_px_size,
                                     downscale=1
         )
         self.assertIsInstance(data, np.ndarray)
-        self.assertTrue(data.shape==raw_img.shape)
-        self.assertTrue(np.array_equal(data, raw_img))
+        self.assertEqual(data.shape, self.test_data.shape)
+        self.assertTrue(np.array_equal(data, self.test_data))
 
         # Test whether image has been correctly binned
         data_ds, px_size_ds = io.read_mrc(fname="./test.mrc",
                                           px_size_nm=test_px_size,
                                           downscale=2
         )
-        self.assertTrue(data_ds.shape==(50, 50, 50))
+        self.assertEqual(data_ds.shape, (50, 50, 50))
         self.assertAlmostEqual(px_size_ds, 2.0, places=7)
 
-        tmpdir.cleanup()
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+
+    def tearDown(self):
+        self.tmpdir.cleanup()
